8a5c1040101cf5d290451353a7d5b2ab15d9d818
MuhammadSalmanc <sabsalman14@gmail.com>
2022-03-15 20:49:05 +0500

    Initial Commit

---
+++ src/User.java
@@ -0,0 +1,15 @@
+public class User {
+    private int id;
+    private String name;
+    private int age;
+    private String phone;
+    private int creditCardId;
+
+    public static void main(String[] args) {
+        System.out.println("+++++++++++++Welcome to BAV AirLines\"+++++++++++++\nFor Further proceeding, Please Login. ");
+        System.out.println("\t\t(a) Press 1 to Login.");
+        System.out.println("\t\t(b) Press 2 to Register.");
+        System.out.println("\t\t(c) Press 3 to Book a Ticket.");
+        System.out.println("\t\t(d) Press 4 to Reserve a Ticket.\t\t\t\t\t\t*****Default Username && Password is root-root*****");
+    }
+}
